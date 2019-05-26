7a74bdd5197ad21fc8ef3f940ad01824ba482b21
KimJaei <48463426+KimJaei@users.noreply.github.com>
2019-05-26 17:24:59 +0900

    한국인의 삶을 파악하라! - 연령대 및 성별에 따른 월급

---
+++ five.R
@@ -0,0 +1,19 @@
+sex_income <- welfare %>%
+  filter(!is.na(income)) %>%
+  group_by(ageg, sex) %>%
+  summarise(maen_income=mean(income))
+
+sex_income
+
+ggplot(data=sex_income, aes(x=ageg, y=maen_income, fill=sex)) +
+  geom_col(position = "dodge") +
+  scale_x_discrete(limits=c("young", "middle", "old"))
+
+sex_age <- welfare %>%
+  filter(!is.na(income)) %>%
+  group_by(age, sex) %>%
+  summarise(mean_income=mean(income))
+
+head(sex_age)
+
+ggplot(data=sex_age, aes(x=age, y=mean_income, col=sex)) + geom_line()
