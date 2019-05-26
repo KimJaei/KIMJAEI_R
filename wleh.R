9b383793cf2829500a2084fc9a4906b71be6491a
KimJaei <48463426+KimJaei@users.noreply.github.com>
2019-05-26 23:15:25 +0900

    지도

---
+++ wleh.R
@@ -0,0 +1,21 @@
+install.packages("ggiraphExtra")
+library(ggiraphExtra)
+
+str(USArrests)
+
+head(USArrests)
+
+library(tibble)
+
+crime <- rownames_to_column(USArrests, var="state")
+crime$state <- tolower(crime$state)
+
+str(crime)
+
+library(ggplot2)
+states_map<-map.data("state")
+str(states_map)
+
+ggChoropleth(data=crime, aes(fill=Murder, map_id=state), map=states_map)
+
+ggChoropleth(data=crime, aes(fill=Murder, map_id=state), map=states_map, interactive = T)
